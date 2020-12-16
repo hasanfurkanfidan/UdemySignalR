aeafc00f93f8baf09541f701b7a74ee06a012d58
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-16 17:42:54 +0300

    Add project files.

---
+++ UdemySignalR.Server/Hubs/MyHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemySignalR.Server.Hubs
+{
+    public class MyHub
+    {
+    }
+}
