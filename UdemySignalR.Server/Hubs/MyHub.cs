5a8dc1e09f9d80786127562b27b8712bd5a0112f
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-16 18:03:05 +0300

    My hub class added

--- UdemySignalR.Server/Hubs/MyHub.cs
+++ UdemySignalR.Server/Hubs/MyHub.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace UdemySignalR.Server.Hubs
 {
-    public class MyHub
+    public class MyHub:Hub
     {
+        public static List<string> Names { get; set; } = new List<string>();
+        //static olması lazım çünkü her istekte listin boşalmaması önemli
+        public async Task SendName(string name)
+        {
+           Names.Add(name);
+           await Clients.All.SendAsync("ReceiveName",name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("RecieveNames", Names);
+            
+        }
+
     }
 }
--- UdemySignalR.Server/Startup.cs
+++ UdemySignalR.Server/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemySignalR.Server.Hubs;
 
 namespace UdemySignalR.Server
 {
@@ -26,6 +27,8 @@ namespace UdemySignalR.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +40,6 @@ namespace UdemySignalR.Server
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
@@ -45,6 +47,7 @@ namespace UdemySignalR.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<MyHub>("/MyHub");
             });
         }
     }
